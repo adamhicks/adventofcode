3b4f70a2625e76e34839096204df040815339406
Adam <adam.frank.hicks@gmail.com>
2022-12-10 10:16:24 +0000

    cathode ray tube

---
+++ 2022/day10/solution.py
@@ -0,0 +1,201 @@
+from itertools import cycle
+from typing import List
+from os import path
+
+def run_program(p: List[str]):
+    x = 1
+    for l in p:
+        yield x
+        if l == "noop":
+            continue
+        _, v = l.split()
+        yield x
+        x += int(v)
+    yield x
+
+def part1(i: List[str]) -> int:
+    return sum(
+        (idx+1)*v 
+        for idx, v in enumerate(run_program(i))
+        if (idx+1) % 40 == 20
+    )
+
+def part2(i: List[str]) -> str:
+    p = run_program(i)
+    return '\n'.join(
+        ''.join(
+            "#" if abs(v - i) <= 1 else "."
+            for i, v in zip(range(40), p)
+        ) for _ in range(6)
+    )
+
+def default_input() -> str:
+    fn = path.join(path.dirname(__file__), "input.txt")
+    with open(fn) as i:
+        return parse(i.read())
+
+def parse(i: str) -> List[str]:
+    return i.splitlines()
+
+def run():
+    i = default_input()
+    print(part1(i))
+    print(part2(i))
+
+def test():
+    tiny = parse("""noop
+addx 3
+addx -5""")
+    print(list(enumerate(run_program(tiny))))
+
+    i = parse("""addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop""")
+    print(part1(i))
+    print(part2(i))
+
+if __name__ == "__main__":
+    test()
