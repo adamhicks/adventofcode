1a23ba9c4368bbd1c415d9cd0256735f728583be
Adam Hicks <adam@luno.com>
2023-12-11 08:49:28 +0000

    day 11, galaxy distances

---
+++ 2023/day11/solution.go
@@ -0,0 +1,130 @@
+package day11
+
+import (
+	_ "embed"
+	"fmt"
+	"strings"
+)
+
+//go:embed input.txt
+var inputString string
+
+type input []string
+
+func parseInput(s string) input {
+	return strings.Split(strings.TrimSpace(s), "\n")
+}
+
+var testString1 = `...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....`
+
+type coord struct {
+	X, Y int
+}
+
+func abs(a int) int {
+	if a > 0 {
+		return a
+	} else {
+		return -a
+	}
+}
+
+func distance(a, b coord) int {
+	return abs(b.Y-a.Y) + abs(b.X-a.X)
+}
+
+func blanksCrossed(a, b int, counts []int) int {
+	var zeroes int
+
+	if a > b {
+		a, b = b, a
+	}
+
+	for _, v := range counts[a:b] {
+		if v == 0 {
+			zeroes++
+		}
+	}
+	return zeroes
+}
+
+func parseMap(s input) ([]coord, []int, []int) {
+	rowCount := make([]int, len(s))
+	colCount := make([]int, len(s[0]))
+
+	var gals []coord
+	for y, l := range s {
+		for x, r := range l {
+			if r != '#' {
+				continue
+			}
+			rowCount[y]++
+			colCount[x]++
+			gals = append(gals, coord{x, y})
+		}
+	}
+	return gals, rowCount, colCount
+}
+
+func runPartOne(s input) error {
+	gals, rowCount, colCount := parseMap(s)
+
+	var sum int
+	for i := 0; i < len(gals); i++ {
+		for j := i + 1; j < len(gals); j++ {
+			a, b := gals[i], gals[j]
+			sum += distance(a, b)
+			sum += blanksCrossed(a.Y, b.Y, rowCount)
+			sum += blanksCrossed(a.X, b.X, colCount)
+		}
+	}
+	fmt.Println(sum)
+
+	return nil
+}
+
+var testString2 = testString1
+
+func runPartTwo(s input) error {
+	gals, rowCount, colCount := parseMap(s)
+
+	var sum int
+	for i := 0; i < len(gals); i++ {
+		for j := i + 1; j < len(gals); j++ {
+			a, b := gals[i], gals[j]
+			sum += distance(a, b)
+			sum += blanksCrossed(a.Y, b.Y, rowCount) * 999999
+			sum += blanksCrossed(a.X, b.X, colCount) * 999999
+		}
+	}
+	fmt.Println(sum)
+
+	return nil
+}
+
+type Solution struct{}
+
+func (Solution) TestPart1() error {
+	return runPartOne(parseInput(testString1))
+}
+
+func (Solution) RunPart1() error {
+	return runPartOne(parseInput(inputString))
+}
+
+func (Solution) TestPart2() error {
+	return runPartTwo(parseInput(testString2))
+}
+
+func (Solution) RunPart2() error {
+	return runPartTwo(parseInput(inputString))
+}
